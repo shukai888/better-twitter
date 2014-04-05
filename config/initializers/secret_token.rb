86e083f51d4bf8a011e411b1e22cebd54408a907
shukai888 <shukai888@gmail.com>
2014-04-05 15:23:02 +0800

    init better-twitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterTwitter::Application.config.secret_token = '9e4a0a24463a45d53b7429c0fdf9c34a733bc1f0cbf8347dc9e013298386e3746945a4146787e9a973a021e5b5a6630885aab9fce30bb2e34e61c53bb5f643a2'
